db005f0f6d03532d5c3dba51cc4da3df49ed11b3
Francesca Ahumada <javiera.francesca@gmail.com>
2021-07-24 10:05:44 -0400

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'restaurant/new'
+  get 'restaurant/edit'
+  get 'restaurant/create'
+  get 'restaurant/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources : restaurants
 end
